29ee96def6de723eb368dfce383aa1ba6a5eadf7
choesoyeon <ssssso.yyyy@gmail.com>
2025-03-25 18:48:49 +0900

    :tada: Initial commit

---
+++ src/test/java/com/example/bakerymes/BakeryMesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bakerymes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryMesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
