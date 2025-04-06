82557f366fc550b5ad2184d63830750ff3037350
choesoyeon <ssssso.yyyy@gmail.com>
2025-04-06 20:23:52 +0900

    :white_check_mark: 수주

---
+++ src/main/java/com/example/bakerymes/model/OrderRequirement.java
@@ -0,0 +1,31 @@
+package com.example.bakerymes.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class OrderRequirement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_item_id")
+    @JsonIgnore // 순환 참조 방지
+    private OrderItem orderItem;
+
+    @ManyToOne
+    private Material material;
+
+    private Double requiredQuantity;
+}
